4a306ee3820e9cd2312bbcc7353a3d8985192867
MeetPatel2403 <meetnavin011@gmail.com>
2024-05-12 14:53:16 +0530

    Add files via upload

---
+++ greenhorns/swap.c
@@ -0,0 +1,44 @@
+// #include <stdio.h>
+
+// int main() {
+//     // Write C code here
+//     printf("*\n");
+//     printf("**\n");
+//      printf("***\n");
+//      printf("****\n");
+//      printf("*****\n");
+//      printf("******\n");
+     
+
+    
+
+
+//     return 0;
+// }
+// 
+// #include <stdio.h>
+
+// int main() {
+//     // Write C code here
+//      printf("******\n");
+//      printf("*   *\n");
+//      printf("*  *\n");
+//      printf("* *\n");
+//     printf("**\n");
+//     printf("*\n");
+     
+//   return 0;
+// }
+
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("    *\n");
+    printf("   * *\n");
+    printf("  * * *\n");
+    printf(" * * * *\n");
+    printf("* * * * *\n");
+     
+  return 0;
+}

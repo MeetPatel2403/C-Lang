4101c5de11e81f6b7edb6de2dfa10eae7b9da8d2
MeetPatel2403 <meetnavin011@gmail.com>
2024-05-12 20:12:08 +0530

    Add files via upload

---
+++ greenhorns/swap.c
@@ -0,0 +1,75 @@
+// first program
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("*\n");
+    printf("**\n");
+     printf("***\n");
+     printf("****\n");
+     printf("*****\n");
+     printf("******\n");
+     return 0;
+}
+
+// second program
+#include <stdio.h>
+
+void main() {
+    // Write C code here
+     printf("******\n");
+     printf("*****\n");
+     printf("****\n");
+     printf("***\n");
+     printf("**\n");
+     printf("*\n");
+
+#include <stdio.h>
+
+
+// third program
+int main() {
+    // Write C code here
+     printf("******\n");
+     printf("*   *\n");
+     printf("*  *\n");
+     printf("* *\n");
+    printf(" **\n");
+    printf(" *\n");
+     
+  return 0;
+}
+
+// four program 
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("    *\n");
+    printf("   * *\n");
+    printf("  * * *\n");
+    printf(" * * * *\n");
+    printf("* * * * *\n");
+     
+  return 0;
+}
+
+// fifth program 
+#include<stdio.h>
+void main(){
+  printf("    *\n");
+  printf("   * *\n");
+  printf("  *   *\n");
+  printf(" *     *\n");
+  printf("* * * * *\n");
+}
+
+// six program 
+#include<stdio.h>
+void main(){
+  printf("* * * * *\n");
+  printf(" *     *\n");
+  printf("  *   *\n");
+  printf("   * *\n");
+  printf("    *\n");
+}
